abfc0ce93aae56def1f65cb48666d6d2c01e40af
Jose Alvarez <5135@holbertonschool.com>
2022-08-23 10:07:03 -0700

    bubblerino

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - Bubblerino Sorterino
+ * @array: array
+ * @size: size of array
+ * Return: Nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j;
+	int tmp = 0;	
+
+	for (; i <= size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if(array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+		}	}
+	}
+}
