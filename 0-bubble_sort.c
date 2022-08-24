fd19cf45fad7231d9e4663c4c51a9610585444f8
Jose Alvarez <5135@holbertonschool.com>
2022-08-23 18:45:10 -0700

    0-bubble pero mas rapido

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,9 @@ void bubble_sort(int *array, size_t size)
 
 	if (!array)
 		return;
-	for (; i <= size; i++)
+	for (; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
