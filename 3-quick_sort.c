225d1857661e03026db14652ca80b9bf70f8bcf3
C-Mauro <maurocamila744@gmail.com>
2022-08-26 16:18:03 -0700

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,44 +1,44 @@
 #include "sort.h"
-/**
- * quick_sort - main
- * @array: array
- * @size: size
- * Return: Void
- */
- void quick_sort(int *array, size_t size)
-{
-	int end = 0, start = 0, i = 0;
-	int partition;
-	
-	while(array[i])
-		end++;
-		i++;
 
-	if (start < end)
+void swap(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int array[], int low, int high, int realsize)
+{  
+	int pivot = array[high];
+	int i = (low - 1), j;
+
+	for (j = low; j < high; j++)
 	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, realsize);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	print_array(array, realsize);
+	return (i + 1);
+}
+
+void realquickSort(int array[], int low, int high, int size)
+{
+	int pi, realsize = size;
+  
+	if (low < high)
+	{
+		pi = partition(array, low, high, realsize);
+		realquickSort(array, low, pi - 1, realsize);
+		realquickSort(array, pi + 1, high, realsize);
 	}
 }
 
-/**
- * partition_lomutosky - main
- * @array: array
- * start: start of array
- * end: end of array
- * Return: index
- */
-int partition_lomutosky(int **array, int start, int end)
+void quick_sort(int *array, size_t size)
 {
-	int a = *array;
-	int pivot = *array[end]; /* Hay que sacar high*/
-	int i = (low - 1), int j; /* Index del elemento mas pequeño*/
-	for (j = start; j <= end - 1; j++)
-	{
-		if (a[j] <= pivot)
-		{
-		i++;
-		/**swapeamos*/
-		}
-	}
-	/**swapeamos de nuevo*/
-	return (i + 1);
+	realquickSort(array, 0, size - 1, size);
 }
