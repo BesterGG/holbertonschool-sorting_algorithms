c8c0d70f61076a37f6611961eb24008e2d72bd9a
Camila Mauro <105792882+C-Mauro@users.noreply.github.com>
2022-08-26 21:02:34 -0300

    quick sort (#1)
    task 3, finished.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
-
+/**
+ *swap- swap the values of in the nodes.
+ *@a: a value
+ *@b: b value
+ *Return: Nothing
+ */
 void swap(int *a, int *b)
 {
 	int t = *a;
@@ -7,8 +12,16 @@ void swap(int *a, int *b)
 	*b = t;
 }
 
+/**
+ *partition- partition lomuto implementation
+ *@array: array to be sorted
+ *@low: the first value on the array
+ *@high: the last element on the array
+ *@realsize: size of the array
+ *Return: value of
+ */
 int partition(int array[], int low, int high, int realsize)
-{  
+{
 	int pivot = array[high];
 	int i = (low - 1), j;
 
@@ -25,11 +38,18 @@ int partition(int array[], int low, int high, int realsize)
 	print_array(array, realsize);
 	return (i + 1);
 }
-
+/**
+ *realquickSort -sort array
+ *@array: the array to be sorted
+ *@low: the first element on the array
+ *@high: the last element on the array
+ *@size: lenght of the array
+ *Return: Nothing
+ */
 void realquickSort(int array[], int low, int high, int size)
 {
 	int pi, realsize = size;
-  
+
 	if (low < high)
 	{
 		pi = partition(array, low, high, realsize);
@@ -37,7 +57,12 @@ void realquickSort(int array[], int low, int high, int size)
 		realquickSort(array, pi + 1, high, realsize);
 	}
 }
-
+/**
+ *quick_sort -sorting an array
+ *@array: the array
+ *@size: lenght of the array
+ *Return: nothing
+ */
 void quick_sort(int *array, size_t size)
 {
 	realquickSort(array, 0, size - 1, size);
